6efd2a735d62276136bbc62351f74226fe2df3a6
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-03-11 19:54:51 -0500

    Add solution for grouping anagrams

---
+++ NEETCODE 150/49.py
@@ -0,0 +1,11 @@
+def groupAnagrams(arr):
+    res = {}
+    for i in arr:
+        sorted_i = "".join(sorted(i))
+        if sorted_i in res:
+            res[sorted_i].append(i)
+        else:
+            res[sorted_i] = [i]
+    return list(res.values())
+
+print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
